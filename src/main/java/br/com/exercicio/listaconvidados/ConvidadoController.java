db0c526e7c378a82d3997f97c880aa508bd992bf
Lucas <lukkitaalves6@gmail.com>
2021-05-02 20:20:57 -0300

    Insert de Convidados

--- src/main/java/br/com/exercicio/listaconvidados/ConvidadoController.java
+++ src/main/java/br/com/exercicio/listaconvidados/ConvidadoController.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 @Controller
 public class ConvidadoController {
@@ -16,15 +20,31 @@ public class ConvidadoController {
     }
 
     @Autowired
-    private ConvidadoRepository repository;
+    private ConvidadoRepository convidadorepository;
 
     @RequestMapping("listaconvidados")
     public String listaConvidados(Model model){
 
-        Iterable<Convidado> convidados = repository.findAll();
+        Iterable<Convidado> convidados = convidadorepository.findAll();
         model.addAttribute("convidados", convidados);
 
         return "listaconvidados";
     }
+
+    @RequestMapping(value = "salvar", method = RequestMethod.POST)
+    public String salvar(@RequestParam("nome") String nome,
+                         @RequestParam("email") String email,
+                         @RequestParam("telefone") String telefone, Model model){
+
+        Convidado convidado = new Convidado(nome,email,telefone);
+
+        convidadorepository.save(convidado);
+        Iterable<Convidado> convidados = convidadorepository.findAll();
+
+        model.addAttribute("convidados", convidados);
+
+        return "listaconvidados";
+    }
+
 
 }
